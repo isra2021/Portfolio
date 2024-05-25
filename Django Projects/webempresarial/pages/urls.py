0c07234b7304b19fd66a22e4f9454d804fb71207
isra2021 <egancarry@gmail.com>
2024-05-25 04:06:58 -0300

    Eliminando cosas irrelevantes

--- Django Projects/webempresarial/pages/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('<int:page_id>/', views.page, name="page"),
-]
--- React Projects/taskapp/src/components/TaskBanner.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const TaskBanner = (props) =>
-    <h4 className="bg-primary text-white text-center p-4">
-        {props.userName} Task app ({props.taskItems.filter(t => !t.done).length} tasks to do)
-    </h4>
-
-export default TaskBanner
